a33a3bab01a2401fcba2e6a8c08d7768b72e7db4
Chi Zhang <zhangchi.usc1992@bytedance.com>
2025-01-17 00:03:03 +0800

    [misc] fix: fix license (#110)
    - fix license
    - add license ci

--- verl/trainer/ppo/core_algos.py
+++ verl/trainer/ppo/core_algos.py
@@ -1,3 +1,4 @@
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
 # Copyright 2022 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
