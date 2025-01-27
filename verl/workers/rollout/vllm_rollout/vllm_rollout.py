c99df03fc47264b45be5b1ac44ce21f21763b6ac
Xingyao Wang <xingyao6@illinois.edu>
2025-01-26 23:42:25 -0500

    [VLLM] Set max_num_batched_tokens for vllm rollout (#140)
    We set `max_num_batched_tokens` in config `.rollout`, but they weren't
    actually being passed to VLLM -- causing potential insufficient use of
    GPUs.
    
    This PR:
    
    - properly pass `max_num_batched_tokens` from config to vLLM
    - set `disable_log_stats` to False, so vLLM performance information can
    be properly displayed (to spot issues)

--- verl/workers/rollout/vllm_rollout/vllm_rollout.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout.py
@@ -74,6 +74,7 @@ class vLLMRollout(BaseRollout):
         tensor_parallel_size = self.config.get('tensor_model_parallel_size', 1)
         assert tensor_parallel_size <= torch.distributed.get_world_size(), \
             "tensor parallel size should be less than or equal to the world size"
+        max_num_batched_tokens = self.config.get('max_num_batched_tokens', 8192)
 
         if kwargs.get('train_tp', None) is not None:
             # deployed with megatron
@@ -88,16 +89,20 @@ class vLLMRollout(BaseRollout):
 
         assert model_hf_config.max_position_embeddings >= config.prompt_length + config.response_length, \
             "model context length should be greater than total sequence length"
-        self.inference_engine = LLM(actor_module,
-                                    tokenizer=tokenizer,
-                                    model_hf_config=model_hf_config,
-                                    tensor_parallel_size=tensor_parallel_size,
-                                    dtype=config.dtype,
-                                    enforce_eager=config.enforce_eager,
-                                    gpu_memory_utilization=config.gpu_memory_utilization,
-                                    skip_tokenizer_init=False,
-                                    max_model_len=config.prompt_length + config.response_length,
-                                    load_format=config.load_format)
+        self.inference_engine = LLM(
+            actor_module,
+            tokenizer=tokenizer,
+            model_hf_config=model_hf_config,
+            tensor_parallel_size=tensor_parallel_size,
+            dtype=config.dtype,
+            enforce_eager=config.enforce_eager,
+            gpu_memory_utilization=config.gpu_memory_utilization,
+            skip_tokenizer_init=False,
+            max_model_len=config.prompt_length + config.response_length,
+            load_format=config.load_format,
+            disable_log_stats=False,
+            max_num_batched_tokens=max_num_batched_tokens,
+        )
 
         # Offload vllm model to reduce peak memory usage
         self.inference_engine.offload_model_weights()
