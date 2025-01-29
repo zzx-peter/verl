e3cc0ebd7381611445cd2d3f15e251b15fb47ca0
HL <linhaibin.eric@gmail.com>
2025-01-28 21:38:47 -0800

    fix: fix missing version file dependency when installing without -e (#156)

--- verl/single_controller/__init__.py
+++ verl/single_controller/__init__.py
@@ -16,5 +16,5 @@ import os
 
 version_folder = os.path.dirname(os.path.join(os.path.abspath(__file__)))
 
-with open(os.path.join(version_folder, 'version/version')) as f:
+with open(os.path.join(os.path.join(version_folder, os.pardir), 'version/version')) as f:
     __version__ = f.read().strip()
