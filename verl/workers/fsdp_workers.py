679798cd12e2997155624a64d78f879e808339dc
dignfei <913015993@qq.com>
2025-01-31 09:05:04 +0800

    fix bug：fix checkpoint save with existing dirs (#174)
    ![image](https://github.com/user-attachments/assets/f0bae990-a4ef-49da-aa1e-58894b41db5f)
    
    ---------
    
    Co-authored-by: HL <linhaibin.eric@gmail.com>

--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -544,7 +544,7 @@ class ActorRolloutRefWorker(Worker):
             if hdfs_path is not None:
                 print(f'Uploading actor checkpoint to {hdfs_path}')
                 hdfs_io.makedirs(hdfs_path, exist_ok=True)
-                hdfs_io.copy(src=local_path, dst=hdfs_path)
+                hdfs_io.copy(src=local_path, dst=hdfs_path, dirs_exist_ok=True)
 
         torch.distributed.barrier()
         if self._is_offload_param:
