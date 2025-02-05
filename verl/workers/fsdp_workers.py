4669ab9344465a23a6059c0adfd8fe4a04b92b11
Wei Xiong <90632760+WeiXiongUST@users.noreply.github.com>
2025-02-04 20:20:01 -0600

    fix critic save error (#199)
    This PR is similar to PR https://github.com/volcengine/verl/pull/174 but
    fix the critic save error
    
    I move the old PR to this one due to some redundant commit

--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -816,7 +816,7 @@ class CriticWorker(Worker):
             if hdfs_path is not None:
                 print(f'Uploading critic checkpoint to {hdfs_path}')
                 hdfs_io.makedirs(hdfs_path, exist_ok=True)
-                hdfs_io.copy(src=local_path, dst=hdfs_path)
+                hdfs_io.copy(src=local_path, dst=hdfs_path, dirs_exist_ok=True)
 
         torch.distributed.barrier()
         if self._is_offload_param:
