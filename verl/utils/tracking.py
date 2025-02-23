7a128c1cd804180d96b54406f2ad26d5d9cc0859
liudayuan-carrot <liudayuan@abigcarrot.com>
2025-02-23 19:28:16 +0800

    feat: tracking support vemlp (#339)
    Tracking backend support vemlp wandb
    
    ---------
    
    Co-authored-by: liudayuan.carrot <liudayuan.carrot@bytedance.com>

--- verl/utils/tracking.py
+++ verl/utils/tracking.py
@@ -22,7 +22,7 @@ from typing import List, Union, Dict, Any
 
 
 class Tracking(object):
-    supported_backend = ["wandb", "mlflow", "swanlab", "console"]
+    supported_backend = ["wandb", "mlflow", "swanlab", "vemlp_wandb", "console"]
 
     def __init__(self, project_name, experiment_name, default_backend: Union[str, List[str]] = 'console', config=None):
         if isinstance(default_backend, str):
@@ -63,6 +63,24 @@ class Tracking(object):
                          mode=SWANLAB_MODE)
             self.logger["swanlab"] = swanlab
 
+        if 'vemlp_wandb' in default_backend:
+            import os
+            import volcengine_ml_platform
+            from volcengine_ml_platform import wandb as vemlp_wandb
+            volcengine_ml_platform.init(
+                ak=os.environ["VOLC_ACCESS_KEY_ID"],
+                sk=os.environ["VOLC_SECRET_ACCESS_KEY"],
+                region=os.environ["MLP_TRACKING_REGION"],
+            )
+
+            vemlp_wandb.init(
+                project=project_name,
+                name=experiment_name,
+                config=config,
+                sync_tensorboard=True,
+            )
+            self.logger['vemlp_wandb'] = vemlp_wandb
+
         if 'console' in default_backend:
             from verl.utils.logger.aggregate_logger import LocalLogger
             self.console_logger = LocalLogger(print_to_console=True)
@@ -78,6 +96,8 @@ class Tracking(object):
             self.logger['wandb'].finish(exit_code=0)
         if 'swanlab' in self.logger:
             self.logger['swanlab'].finish()
+        if 'vemlp_wandb' in self.logger:
+            self.logger['vemlp_wandb'].finish(exit_code=0)
 
 
 class _MlflowLoggingAdapter:
