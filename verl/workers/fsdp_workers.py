a0f05da89576ff5cdfe17873582d6e47c4bee87d
Chi Zhang <zhangchi.usc1992@bytedance.com>
2025-02-27 20:16:44 +0800

    [ckpt] fix: fix oom when resume from ckpt (#402)

--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -576,6 +576,9 @@ class ActorRolloutRefWorker(Worker):
         if self._is_offload_param:
             offload_fsdp_model_to_cpu(self.actor_module_fsdp)
 
+        if self._is_offload_optimizer:
+            offload_fsdp_optimizer(self.actor_optimizer)
+
 
 class CriticWorker(Worker):
 
@@ -847,6 +850,9 @@ class CriticWorker(Worker):
         torch.distributed.barrier()
         if self._is_offload_param:
             offload_fsdp_model_to_cpu(self.critic_module)
+
+        if self._is_offload_optimizer:
+            offload_fsdp_optimizer(self.critic_optimizer)
 
 
 # TODO(sgm): we may need to extract it to dp_reward_model.py
