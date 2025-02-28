6e4a445f2e0a78643de347fcb00a091410c7dd24
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-02-28 22:46:45 +0800

    [Feature] Assert Single Batch for `val_dataloader` (#424)
    This is an enhancement for the single batch strategy for
    `val_dataloader`, making https://github.com/volcengine/verl/pull/353
    more robust.

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -528,10 +528,11 @@ class RayPPOTrainer(object):
             collate_fn=collate_fn)
 
         assert len(self.train_dataloader) >= 1
-        assert len(self.val_dataloader) >= 1
+        assert len(
+            self.val_dataloader
+        ) == 1, "Validation dataloader must have a single batch, which inference engines will schedule the memory themselves."
 
         print(f'Size of train dataloader: {len(self.train_dataloader)}')
-        print(f'Size of val dataloader: {len(self.val_dataloader)}')
 
         # inject total_training_steps to actor/critic optim_config. This is hacky.
         total_training_steps = len(self.train_dataloader) * self.config.trainer.total_epochs
