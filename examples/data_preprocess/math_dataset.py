591a476dbf5b52028bb6176235664d13dedffddd
Alexander Liu <56422865+alexanderliu-creator@users.noreply.github.com>
2025-03-26 21:15:32 +0800

    Update math_dataset.py to fix typo in the annotation (#765)
    The dataset name is MATH-lighteval instead of GSM8k

--- examples/data_preprocess/math_dataset.py
+++ examples/data_preprocess/math_dataset.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Preprocess the GSM8k dataset to parquet format
+Preprocess the MATH-lighteval dataset to parquet format
 """
 
 import os
