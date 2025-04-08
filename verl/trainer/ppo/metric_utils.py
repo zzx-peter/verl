6433fd4a97ef2480835466a461290a92cf85f307
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-04-08 16:28:43 +0800

    fix: return list from bootstrap_metric (#969)
    Fixing #950

--- verl/trainer/ppo/metric_utils.py
+++ verl/trainer/ppo/metric_utils.py
@@ -245,20 +245,20 @@ def process_validation_metrics(data_sources: list[str],
 
                 for n in ns:
                     # Best/Worst-of-N
-                    (bon_mean, bon_std), (won_mean, won_std) = bootstrap_metric(data=var_vals,
-                                                                                subset_size=n,
-                                                                                reduce_fns=[np.max, np.min],
-                                                                                seed=seed)
+                    [(bon_mean, bon_std), (won_mean, won_std)] = bootstrap_metric(data=var_vals,
+                                                                                  subset_size=n,
+                                                                                  reduce_fns=[np.max, np.min],
+                                                                                  seed=seed)
                     metric[f"best@{n}/mean"], metric[f"best@{n}/std"] = bon_mean, bon_std
                     metric[f"worst@{n}/mean"], metric[f"worst@{n}/std"] = won_mean, won_std
                     # Majority voting
                     if var2vals.get("pred", None) is not None:
                         vote_data = [{"val": val, "pred": pred} for val, pred in zip(var_vals, var2vals["pred"])]
-                        (maj_n_mean, maj_n_std) = bootstrap_metric(
-                            data=vote_data,
-                            subset_size=n,
-                            reduce_fns=[partial(calc_maj_val, vote_key="pred", val_key="val")],
-                            seed=seed)
+                        [(maj_n_mean, maj_n_std)
+                        ] = bootstrap_metric(data=vote_data,
+                                             subset_size=n,
+                                             reduce_fns=[partial(calc_maj_val, vote_key="pred", val_key="val")],
+                                             seed=seed)
                         metric[f"maj@{n}/mean"], metric[f"maj@{n}/std"] = maj_n_mean, maj_n_std
 
                 data_src2prompt2var2metric[data_source][prompt][var_name] = metric
