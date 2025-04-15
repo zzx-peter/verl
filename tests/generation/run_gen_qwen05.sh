189c87c37cacb207ca1a64305f638434810d83e9
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-04-15 22:27:27 +0800

    [CI] feat: try HF_HUB_OFFLINE to fix network errors (#1098)
    Trying to fix network errors like
    
    ```
    huggingface_hub.errors.HfHubHTTPError: 429 Client Error: Too Many Requests for url: ...
    ```

--- tests/generation/run_gen_qwen05.sh
+++ tests/generation/run_gen_qwen05.sh
@@ -1,33 +1,26 @@
+#!/usr/bin/env bash
 # Tested with 1 & 4 GPUs
 set -xeuo pipefail
 
-if [ "$#" -lt 2 ]; then
-    echo "Usage: run_gen_qwen05.sh <nproc_per_node> <save_path> [other_configs...]"
-    exit 1
-fi
+MODEL_ID=${MODEL_ID:-Qwen/Qwen2.5-0.5B-Instruct}
 
-nproc_per_node=$1
-save_path=$2
-infer_tp=${3:-2}  # Default tensor parallel size to 2
-
-# Shift the arguments so $@ refers to the rest
-shift 2
-
-huggingface-cli download Qwen/Qwen2.5-0.5B-Instruct --local-dir $HOME/models/Qwen/Qwen2.5-0.5B-Instruct
+NGPUS_PER_NODE=${NGPUS_PER_NODE:-4}
+OUTPUT_PATH=${OUTPUT_PATH:-$HOME/data/gen/qwen_05_gen_test.parquet}
+GEN_TP=${GEN_TP:-2}  # Default tensor parallel size to 2
 
 python3 -m verl.trainer.main_generation \
     trainer.nnodes=1 \
-    trainer.n_gpus_per_node=$nproc_per_node \
-    data.path=$HOME/data/gsm8k/test.parquet \
+    trainer.n_gpus_per_node="${NGPUS_PER_NODE}" \
+    data.path="${HOME}/data/gsm8k/test.parquet" \
     data.prompt_key=prompt \
     data.n_samples=1 \
-    data.output_path=$save_path \
-    model.path=$HOME/models/Qwen/Qwen2.5-0.5B-Instruct \
+    data.output_path="${OUTPUT_PATH}" \
+    model.path="${MODEL_ID}" \
     +model.trust_remote_code=True \
     rollout.temperature=1.0 \
     rollout.top_k=50 \
     rollout.top_p=0.7 \
     rollout.prompt_length=2048 \
     rollout.response_length=1024 \
-    rollout.tensor_model_parallel_size=$infer_tp \
+    rollout.tensor_model_parallel_size="${GEN_TP}" \
     rollout.gpu_memory_utilization=0.8
