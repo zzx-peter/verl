ba38413aa55f38ba069de4a2ef3429c65a9fe029
Mert Unsal <mertunsal1905@gmail.com>
2025-04-28 14:17:42 +0200

    Option to make model private when pushing to hub, pushing the tokenizer for convenience (#1259)
    Very small changes to `model_merger.py` so that tokenizer is pushed to
    hub and model can be pushed privately.

--- scripts/model_merger.py
+++ scripts/model_merger.py
@@ -27,6 +27,7 @@ from transformers import (
     AutoModelForCausalLM,
     AutoModelForTokenClassification,
     AutoModelForVision2Seq,
+    AutoTokenizer,
     GenerationConfig,
 )
 
@@ -45,10 +46,7 @@ parser.add_argument(
     "--local_dir",
     type=str,
     required=True,
-    help=(
-        "The path for your saved model. For megatron, point to the base dir of model, rng, optimizer checkpoints, "
-        "commonly be `config.default_local_dir/global_step_\{global_step\}`."
-    ),
+    help=("The path for your saved model. For megatron, point to the base dir of model, rng, optimizer checkpoints, commonly be `config.default_local_dir/global_step_\{global_step\}`."),
 )
 parser.add_argument("--target_dir", required=False, default="tmp", type=str, help="The path for the target model")
 parser.add_argument("--hf_upload_path", default=False, type=str, help="The path of the huggingface repo to upload")
@@ -59,6 +57,8 @@ parser.add_argument(
     required=False,
     help="test correctness of hf_model, , with hf_model in checkpoint.contents",
 )
+parser.add_argument("--private", required=False, default=False, help="Whether to upload the model to private repo")
+
 args = parser.parse_args()
 os.makedirs(args.target_dir, exist_ok=True)
 if args.test:
@@ -81,7 +81,7 @@ def upload_model_to_huggingface(hf_path):
     from huggingface_hub import HfApi
 
     api = HfApi()
-    api.create_repo(repo_id=args.hf_upload_path, private=False, exist_ok=True)
+    api.create_repo(repo_id=args.hf_upload_path, private=args.private, exist_ok=True)
     api.upload_folder(folder_path=hf_path, repo_id=args.hf_upload_path, repo_type="model")
 
 
@@ -96,10 +96,7 @@ def patch_model_generation_config(model, hf_model_path):
         try:
             model.generation_config = GenerationConfig.from_pretrained(args.hf_model_path)
         except OSError:
-            print(
-                f"Warning: Generation config file not found in {args.hf_model_path}, "
-                "using a generation config created from the model config."
-            )
+            print(f"Warning: Generation config file not found in {args.hf_model_path}, using a generation config created from the model config.")
             pass
     return model
 
@@ -225,6 +222,11 @@ def convert_fsdp_checkpoints_to_hfmodels():
     model.save_pretrained(hf_path, state_dict=state_dict)
     del state_dict
     del model
+
+    print("Saving tokenizer")
+    tokenizer = AutoTokenizer.from_pretrained(args.hf_model_path)
+    tokenizer.save_pretrained(hf_path)
+
     if args.hf_upload_path:
         upload_model_to_huggingface(hf_path)
 
@@ -430,6 +432,11 @@ def convert_megatron_checkpoints_to_hfmodels():
     model.save_pretrained(hf_path, state_dict=state_dict)
     del state_dict
     del model
+
+    print("Saving tokenizer")
+    tokenizer = AutoTokenizer.from_pretrained(args.hf_model_path)
+    tokenizer.save_pretrained(hf_path)
+
     if args.hf_upload_path:
         upload_model_to_huggingface(hf_path)
 
