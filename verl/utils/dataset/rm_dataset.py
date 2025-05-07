Fix for RM Data Attention Mask Bug (#1411)
[BUG] This issue addresses the bug related to the RM data attention
mask, which was also mentioned in a previous
[issue](https://github.com/volcengine/verl/issues/1341). The fix has
been implemented to ensure proper functionality.

verl/utils/dataset/rm_dataset.py
@@ -131,7 +131,7 @@ class RMDataset(Dataset):
         rejected_input_ids, rejected_attention_mask = self._pad_to_length(rejected_input_ids, rejected_attention_mask)
 
         input_ids = torch.stack((chosen_input_ids, rejected_input_ids), dim=0)
-        attention_mask = torch.stack((rejected_input_ids, rejected_attention_mask), dim=0)
+        attention_mask = torch.stack((chosen_attention_mask, rejected_attention_mask), dim=0)
 
         return {
             "input_ids": input_ids,