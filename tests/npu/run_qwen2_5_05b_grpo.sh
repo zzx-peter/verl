54c9b7364c2d188b2ba4107404cfa3c2b446df19
Chunyu <15750543867@163.com>
2025-05-26 15:53:07 +0800

    update ascend_quick_start doc (#1685)
    ### Checklist Before Starting
    
    - [x] Search for similar PR(s).
    
    ### What does this PR do?
    
    update ascend_quick_start.rst
    
    ### High-Level Design
    
    > Demonstrate the high-level design if this PR is complex.
    
    ### Specific Changes
    
    1. rename ascend_quick_start.rst
    2. add the accuracy and throughput data of GRPO.
    
    ### API
    
    > Demonstrate how the API changes if any.
    
    ### Usage Example
    
    > Provide usage example(s) for easier usage.
    
    ```python
    # Add code snippet or script demonstrating how to use this 
    ```
    
    ### Test
    
    > For changes that can not be tested by CI (e.g., algorithm
    implementation, new model support), validate by experiment(s) and show
    results like training curve plots, evaluatuion results, etc.
    
    ### Additional Info.
    
    - **Issue Number**: Fixes issue # or discussion # if any.
    - **Training**: [Note which backend this PR will affect: FSDP, Megatron,
    both, or none]
    - **Inference**: [Note which backend this PR will affect: vLLM, SGLang,
    both, or none]
    
    ### Checklist Before Submitting
    
    - [x] Read the [Contribute
    Guide](https://github.com/volcengine/verl?tab=readme-ov-file#contribution-guide).
    - [x] Apply [pre-commit
    checks](https://github.com/volcengine/verl?tab=readme-ov-file#code-linting-and-formatting).
    - [x] Add `[BREAKING]` to the PR title if it breaks any API.
    - [x] Update the documentation about your changes in the
    [docs](https://github.com/volcengine/verl/tree/main/docs).
    - [x] Add CI test(s) if necessary.

--- tests/npu/run_qwen2_5_05b_grpo.sh
+++ tests/npu/run_qwen2_5_05b_grpo.sh
@@ -12,7 +12,7 @@ python3 -m verl.trainer.main_ppo \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-0.5B-Instruct \
-    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr=5e-7 \
     actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=64 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=20 \
