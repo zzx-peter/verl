7c91b103f519cea220625284d67e4bccf145d147
Guangming Sheng <shengguangming@bytedance.com>
2025-05-28 19:39:49 +0800

    [misc] fix: reduce training iter in spin and sppo ci (#1738)
    ### Checklist Before Starting
    
    - [ ] Search for similar PR(s).
    
    ### What does this PR do?
    
    Reduce training iterations in spin and sppo ci to reduce ci time.
    
    ### Test
    
    SPIN and SPPO CI
    
    ### Additional Info.
    
    No
    
    ### Checklist Before Submitting
    
    - [ ] Read the [Contribute
    Guide](https://github.com/volcengine/verl?tab=readme-ov-file#contribution-guide).
    - [ ] Apply [pre-commit
    checks](https://github.com/volcengine/verl?tab=readme-ov-file#code-linting-and-formatting).
    - [ ] Add `[BREAKING]` to the PR title if it breaks any API.
    - [ ] Update the documentation about your changes in the
    [docs](https://github.com/volcengine/verl/tree/main/docs).
    - [ ] Add CI test(s) if necessary.

--- tests/e2e/run_sppo.sh
+++ tests/e2e/run_sppo.sh
@@ -39,8 +39,9 @@ python3 -m recipe.sppo.main_sppo \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger=['console'] \
-    trainer.val_before_train=True \
+    trainer.val_before_train=False \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
+    trainer.total_training_steps=1 \
     trainer.total_epochs=2 $@
