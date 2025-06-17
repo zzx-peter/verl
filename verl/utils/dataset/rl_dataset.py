992ac065a16c314e8fe7068230289dd7102da939
Lumeng Wu <69505389+dirtyDan0@users.noreply.github.com>
2025-06-18 03:38:13 +0800

    [data] fix: multimodal overlong prompt length filtering (#2063)
    ### Checklist Before Starting
    
    - [x] Searched for similar PR(s).
    - [x] Checked PR Title format
      - In format of: [modules] type: Title
    - modules are in `fsdp, megatron, sglang, vllm, rollout, trainer, ci,
    training_utils, recipe, hardware, deployment, ray, worker,
    single_controller, misc, perf, model, algo, env, tool, ckpt, doc, data`
      - type is in `feat, fix, refactor, chore`
    - can involve multiple modules, seperated by `,` or space, like
    `[megatron, fsdp, doc] feat: xxx`
    
    ### What does this PR do?
    
    Prompt length filtering should utilize the processor when handling
    multimodal inputs.
    
    ### Checklist Before Submitting
    
    - [x] Read the [Contribute
    Guide](https://github.com/volcengine/verl?tab=readme-ov-file#contribution-guide).
    - [x] Apply [pre-commit
    checks](https://github.com/volcengine/verl?tab=readme-ov-file#code-linting-and-formatting).
    - [ ] Add `[BREAKING]` to the PR title `description` if it breaks any
    API.
    - [ ] Update the documentation about your changes in the
    [docs](https://github.com/volcengine/verl/tree/main/docs).
    - [ ] New CI unit test(s) are added to cover the code path.
    - [ ] Rely on existing unit tests on CI that covers the code path.

--- verl/utils/dataset/rl_dataset.py
+++ verl/utils/dataset/rl_dataset.py
@@ -138,9 +138,29 @@ class RLHFDataset(Dataset):
         # filter out too long prompts
         if self.filter_overlong_prompts:
             tokenizer = self.tokenizer
+            processor = self.processor
             prompt_key = self.prompt_key
+            image_key = self.image_key
+            video_key = self.video_key
+
+            if processor is not None:
+                from verl.utils.dataset.vision_utils import process_image, process_video
+
+                def doc2len(doc) -> int:
+                    messages = self._build_messages(doc)
+                    raw_prompt = self.processor.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
+                    images = [process_image(image) for image in messages.pop(image_key)] if image_key in messages else None
+                    videos = [process_video(video) for video in messages.pop(video_key)] if video_key in messages else None
+
+                    return len(processor(text=[raw_prompt], images=images, videos=videos)["input_ids"][0])
+
+            else:
+
+                def doc2len(doc) -> int:
+                    return len(tokenizer.apply_chat_template(doc[prompt_key], add_generation_prompt=True))
+
             self.dataframe = self.dataframe.filter(
-                lambda doc: len(tokenizer.apply_chat_template(doc[prompt_key], add_generation_prompt=True)) <= self.max_prompt_length,
+                lambda doc: doc2len(doc) <= self.max_prompt_length,
                 num_proc=self.num_workers,
                 desc=f"Filtering prompts longer than {self.max_prompt_length} tokens",
             )
@@ -166,7 +186,9 @@ class RLHFDataset(Dataset):
             for message in messages:
                 content = message["content"]
                 content_list = []
-                for segment in re.split("(<image>|<video>)", content):
+                segments = re.split("(<image>|<video>)", content)
+                segments = [item for item in segments if item != ""]
+                for segment in segments:
                     if segment == "<image>":
                         content_list.append({"type": "image"})
                     elif segment == "<video>":
