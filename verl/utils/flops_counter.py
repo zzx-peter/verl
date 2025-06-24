24707f6d4ec9e76bca88b76351637d9563cff418
Yaowei Zheng <hiyouga@buaa.edu.cn>
2025-06-24 16:44:57 +0800

    [model] fix: Revert "[model] feat: Add MiniCPM-o 2.6 support" (#2176)
    Reverts volcengine/verl#1833

--- verl/utils/flops_counter.py
+++ verl/utils/flops_counter.py
@@ -16,7 +16,7 @@ from transformers import PretrainedConfig
 
 from verl.utils.device import get_torch_device
 
-VALID_CONFIG_TYPE = {"llama", "qwen2", "qwen2_vl", "qwen2_5_vl", "qwen3", "qwen3_moe", "deepseek_v3", "minicpmv", "minicpmo"}
+VALID_CONFIG_TYPE = {"llama", "qwen2", "qwen2_vl", "qwen2_5_vl", "qwen3", "qwen3_moe", "deepseek_v3"}
 
 
 def get_device_flops(unit="T"):
@@ -75,8 +75,6 @@ class FlopsCounter:
             "qwen3": self._estimate_qwen2_flops,
             "qwen3_moe": self._estimate_qwen3_moe_flops,
             "deepseek_v3": self._estimate_deepseek_v3_flops,
-            "minicpmv": self._estimate_qwen2_flops,
-            "minicpmo": self._estimate_qwen2_flops,
         }
         self.config = config
 
