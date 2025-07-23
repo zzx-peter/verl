dc1599b7e408f17940d0b6a3a5a5b92fb7af38b9
OC <chenhaiquan@bytedance.com>
2025-07-23 13:09:37 +0800

    [rollout] fix: bug in init_engine Method of AsyncSglangServer (#2664)
    Fix error in AsyncSglangServer.init_engine when find works. The correct
    logic should be based on:
    
    gpu_per_node * nodes = dp_size * tp_size
    
    Also added test steps reported from
    https://github.com/volcengine/verl/issues/2633.

--- verl/workers/rollout/sglang_rollout/async_sglang_server.py
+++ verl/workers/rollout/sglang_rollout/async_sglang_server.py
@@ -47,15 +47,24 @@ class AsyncSGLangServer(AsyncServerBase):
             actor for actor in all_actors if actor.get("name", None).startswith(self.wg_prefix + "WorkerDict_")
         ]
 
+        gpu_per_node = len(set([actor["name"].split(":")[1] for actor in matched_actors]))
+        # total gpu num
+        assert len(matched_actors) == self._dp_size * self._tp_size
+
         for matched_actor in matched_actors:
             fields = matched_actor["name"].split(":")
             assert len(fields) == 2, f"invalid actor name: {matched_actor['name']}"
             pg_index, local_rank = int(fields[0].split("_")[-1]), int(fields[1])
 
-            if (self._dp_size * pg_index + local_rank) // self._tp_size == self._dp_rank:
+            current_global_rank = gpu_per_node * pg_index + local_rank
+            worker_dp_rank = current_global_rank // self._tp_size
+            worker_tp_rank = current_global_rank % self._tp_size
+
+            if worker_dp_rank == self._dp_rank:
                 worker = ray.get_actor(**matched_actor)
                 self.workers.append(worker)
-                if (self._dp_size * pg_index + local_rank) / self._tp_size == self._dp_rank:
+
+                if worker_tp_rank == 0:
                     self.master_worker = worker
 
     async def chat_completion(self, raw_request: Request):
