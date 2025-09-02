9f4f0a5a2579c9c24cb8cc4a2dbdbcb41860fdd4
zzx <22376220@buaa.edu.cn>
2025-09-02 21:37:23 +0800

    调参

--- verl/utils/reward_score/gsm8k.py
+++ verl/utils/reward_score/gsm8k.py
@@ -49,7 +49,7 @@ def extract_solution(solution_str, method="strict"):
     return final_answer
 
 
-def compute_score(solution_str, ground_truth, method="flexible", format_score=0.1, score=1.0):
+def compute_score(solution_str, ground_truth, method="strict", format_score=0.0, score=1.0):
     """The scoring function for GSM8k.
 
     Reference: Trung, Luong, et al. "Reft: Reasoning with reinforced fine-tuning." Proceedings of the 62nd Annual
