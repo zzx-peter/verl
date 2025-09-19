4d094f47448062597fb009b066c2f428ff526b5b
zzx-peter <482341425@qq.com>
2025-09-20 02:19:47 +0800

    aux baseline mini batch should be 32

--- examples/reinforce_plus_plus_aux_loss/run_qwen2.5-1.5b_gsm8k_rf_baseline_aux.sh
+++ examples/reinforce_plus_plus_aux_loss/run_qwen2.5-1.5b_gsm8k_rf_baseline_aux.sh
@@ -27,8 +27,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=$main_model \
     actor_rollout_ref.actor.optim.lr=5e-7 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=16  \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32  \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_coef=0.0001 \
     actor_rollout_ref.actor.kl_loss_type=mse \
@@ -37,12 +37,12 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=8 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     aux_model.enable=True \
     aux_model.model.path=$aux_model \
