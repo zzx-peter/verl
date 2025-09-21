db53ef7cb5e7ffe000d51f974735d5e91cffa9ce
zzx-peter <482341425@qq.com>
2025-09-21 16:24:04 +0800

    2GPUs can solve the problem

--- examples/aux_model_update/run_qwen2.5-1.5b_gsm8k_rf_baseline_aux.sh
+++ examples/aux_model_update/run_qwen2.5-1.5b_gsm8k_rf_baseline_aux.sh
@@ -19,15 +19,15 @@ python3 -m verl.trainer.main_ppo \
     algorithm.kl_penalty=k2 \
     data.train_files="$train_files" \
     data.val_files="$test_files" \
-    data.train_batch_size=64 \
-    data.max_prompt_length=512 \
-    data.max_response_length=512 \
+    data.train_batch_size=128 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=1024 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     actor_rollout_ref.model.path=$main_model \
     actor_rollout_ref.actor.optim.lr=5e-7 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=16  \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32  \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_coef=0.0001 \
@@ -40,7 +40,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=1 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
@@ -51,7 +51,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.val_before_train=False \
     trainer.project_name='qwen2.5_1.5b_3b_gsm8k_update' \
     trainer.experiment_name='aux_model_baseline' \
-    trainer.n_gpus_per_node=1 \
+    trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
     trainer.test_freq=3 \
